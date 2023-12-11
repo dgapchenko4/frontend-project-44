1d41bc06b998cb471660b42bb4603539254a6f84
dgapchenko4 <dgapchenko4@gmail.com>
2023-12-11 23:53:34 +0300

    второй шаг

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!')
+const name = readlineSync.question("May I have your name?");
+console.log(name)
