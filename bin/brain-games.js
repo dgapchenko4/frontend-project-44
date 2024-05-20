616504928dfd01877c95c50674eb07381d7309c3
dgapchenko4 <dgapchenko4@gmail.com>
2024-05-20 13:33:05 +0300

    new333

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,26 +1,5 @@
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-const roundsCount = 3;
+import greetPlayer from '../src/cli.js';
 
-export default (description, getQuestionAndAnswer) => {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-
-  console.log(description);
-
-  for (let i = 0; i < roundsCount; i += 1) {
-    const [question, correctAnswer] = getQuestionAndAnswer();
-    console.log(`Question: ${question}`);
-    const playerAnswer = readlineSync.question('You answer: ');
-
-    if (correctAnswer === playerAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${playerName}`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${playerName}`);
-};
+greetPlayer();
