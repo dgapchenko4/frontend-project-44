8768cae21f29464b2fafcd56e1977c2d5cf70de1
dgapchenko4 <dgapchenko4@gmail.com>
2024-05-28 23:28:14 +0300

    Hello world

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const getRandomNumber = (minRange, maxRange) => {
+  const randomNumber = Math.floor(Math.random() * (maxRange - minRange) + minRange);
+  return randomNumber;
+};
+export const isEvenNumber = (number) => number % 2 === 0;
